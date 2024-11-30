d54cce353549fb6d61f59d6da4cabf7b60109973
Divineleo2001 <divya@atparui.com>
2024-12-01 05:25:57 +0530

    small change

--- src/modules/land-details/data-table.tsx
+++ src/modules/land-details/data-table.tsx
@@ -22,7 +22,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import { Input } from "@/components/ui/input"
+
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
